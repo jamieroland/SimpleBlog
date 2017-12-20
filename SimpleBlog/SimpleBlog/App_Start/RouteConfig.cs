cb5af72cb784d578758718606105f52df53ed1b5
Jamie <jamie.a.roland@gmail.com>
2017-12-20 16:55:53 -0500

    Created initial routing and controllers for posts and users.  Also added the Admin Area with its own MVCs

--- SimpleBlog/SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/SimpleBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlog.Controllers;
 
 namespace SimpleBlog
 {
@@ -11,13 +12,15 @@ namespace SimpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace }; //this is to use the namespace in the normal controllers file for non-admin users, since we have controllers with same names in different places
+            //the below routes use this namespace to find the controllers in the non-admin area
+
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "index" }, namespaces);
         }
     }
 }
